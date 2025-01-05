f9471573e42236a2d8178190931de1ce69822a38
zachariahkozlowski-best <mcfeegles@gmail.com>
2025-01-05 22:46:56 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,144 @@
+import express from 'express';
+import moment from 'moment/moment.js';
+const app = express();
+const PORT = 8000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//* Using the application.
+
+/*
+The app has two API's to interface with.
+The first api is /initiateLottery
+Here you simply set the time until the registration time and time until draw, of which the latter should be greater. The time is measured in minutes.
+
+example api request using body:
+
+{
+  "drawTime":"2",
+  "registerCloseTime": "1"
+}
+
+The second api is how to register users. This api assumes registrants are members of the company and accepts companyIDS (integer).
+To interface with it simply target the /register route
+example api request using the body:
+{"companyID":15}
+
+To use the application you must use the /initiateLottery route first and set a time otherwise you will get a reply stating
+registration is closed.
+
+*/
+
+const interval = 1000;
+let canRegister = false;
+
+const drawees = [];
+const winners = [];
+
+app.get('/', (req, res) => {
+  res.send(' app started successfully');
+});
+
+app.post('/initiateLottery', (req, res) => {
+  canRegister = true;
+  const drawTime = req.body.drawTime; // in minutes
+  const registerCloseTime = req.body.registerCloseTime; // in minutes
+
+  const currentTime = moment();
+  const momentRegisterCloseTime = currentTime
+    .clone()
+    .add(registerCloseTime, 'minutes')
+    .unix();
+  const momentTimeTillDraw = currentTime
+    .clone()
+    .add(drawTime, 'minutes')
+    .unix();
+
+  res
+    .status(200)
+    .send('success countdown timers have begun check your console')
+    .end();
+
+  // Start the interval to update the countdown
+  const intervalId = setInterval(() => {
+    const currentUnixTimestamp = moment().unix();
+    // Calculate remaining time for registration and draw
+    const timeTillDraw = momentTimeTillDraw - currentUnixTimestamp;
+    let registercountDown = showRemaining(
+      currentUnixTimestamp,
+      momentRegisterCloseTime
+    );
+    let drawCountDown = showRemaining(currentUnixTimestamp, momentTimeTillDraw);
+
+    if (registercountDown < 0) {
+      canRegister = false;
+    } else {
+      console.log(
+        'time until registration closes',
+        formatTime(registercountDown)
+      );
+    }
+
+    if (drawCountDown <= 0) {
+      console.log(drawees);
+      const uniqueRegistrants = removeDuplicates(drawees);
+      for (let i = 0; i < 3; i++) {
+        drawWinner(uniqueRegistrants);
+      }
+      console.log(winners);
+    } else {
+      console.log('time till draw', formatTime(drawCountDown));
+    }
+    // Stop the interval when the draw closes
+    if (timeTillDraw <= 0) {
+      clearInterval(intervalId); // Stop the interval
+    }
+  }, interval);
+  res.end();
+});
+
+app.post('/register', (req, res) => {
+  const companyID = req.body.companyID;
+  if (canRegister === false) {
+    res.send('registration is closed');
+    res.end();
+  } else if (drawees.includes(companyID)) {
+    res
+      .send(
+        'Sorry you have already registered, you are only allowed to register once.'
+      )
+      .end();
+  } else {
+    drawees.push(companyID);
+    console.log(drawees);
+    res.end();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on ${PORT}`);
+});
+
+function showRemaining(currentTime, deadlineTime) {
+  const countdown = deadlineTime - currentTime;
+  return countdown;
+}
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes} minutes and ${remainingSeconds} seconds`;
+}
+
+function removeDuplicates(array) {
+  let unique = [...new Set(array)];
+  return unique;
+}
+
+function drawWinner(registrants) {
+  // Select a random index
+  const randomIndex = Math.floor(Math.random() * registrants.length);
+  const winner = registrants[randomIndex];
+  registrants.splice(randomIndex, 1);
+  winners.push(winner);
+}
